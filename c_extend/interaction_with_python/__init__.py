2244bd337f9860ec355e56e95fb7deb8adafea8c
luowenzhuo <luowenzhuo@lbesec.org>
2018-09-10 19:13:31 +0800

    [add]c扩展Python

--- c_extend/interaction_with_python/__init__.py
+++ c_extend/interaction_with_python/__init__.py
@@ -2,6 +2,10 @@ import hello
 import fib
 
 if __name__ == '__main__':
+    #  cookbook:  https://python3-cookbook.readthedocs.io/zh_CN/latest/c15/p01_access_ccode_using_ctypes.html
+    #  https://en.wikibooks.org/wiki/Python_Programming/Extending_with_C
+    #  使用swig更简单[pure c代码]
+    #  使用CFFI
     hello.say_hello('lwz')
     fib.fib(10)
 
